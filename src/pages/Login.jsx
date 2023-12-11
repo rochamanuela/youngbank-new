e59a3474fb673a2c31081f5b07da3639528f1323
rochamanuela <rochadesouzamanuela@gmail.com>
2023-12-11 09:15:59 -0300

    11/12/2023 - 09:15

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,46 @@ import "../components/styles/login.style.css";
 import Footer from "../components/Footer";
 import { Link } from 'react-router-dom';
 
+import { useState } from "react";
+import { useUser } from "../UserContext";
+import axiosInstance from "../services/axiosInstance";
+
 function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch("http://127.0.0.1:8000/api/v1/auth/token/login/", {
+                method: "POST",
+                // headers: {
+                //     "Content-Type": "application/json",
+                // },
+                body: JSON.stringify({ email, password }),
+            });
+
+            if (response.ok) {
+                // Login bem-sucedido, obtém o token da resposta
+                const responseData = await response.json();
+                const token = responseData.token; // Certifique-se de substituir "token" pelo nome real do campo em sua resposta
+
+                // Armazena o token no localStorage ou em algum estado global (você pode usar useContext ou Redux, por exemplo)
+                localStorage.setItem("token", token);
+
+                // Redireciona para a página de dados da conta usando a tag Link
+                return <Link to="/inicial" />;
+            } else {
+                // Tratar erro de login aqui
+                console.error("Erro no login");
+            }
+        } catch (error) {
+            console.error("Erro ao processar a solicitação", error);
+        }
+    };
+
+
     return (
         <div>
             <div className="main-login">
@@ -14,9 +53,26 @@ function Login() {
                     <div className="form">
                         <h1>Login</h1>
                         <p className="text-new">Bem vindo novamente! Faça login para acessar sua conta</p>
-                        <form action="" method="post">
-                            <input type="email" name="email" id="email" placeholder="E-mail" className="custom-input" />
-                            <input type="password" name="senha" id="senha" placeholder="Senha" className="custom-input" />
+                        <form onSubmit={handleSubmit}>
+                            <input
+                                type="email"
+                                name="email"
+                                id="email"
+                                placeholder="E-mail"
+                                className="custom-input"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+
+                            <input type="password"
+                                name="senha"
+                                id="senha"
+                                placeholder="Senha"
+                                className="custom-input"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+
                             <input type="submit" value="Acessar" />
                         </form>
                         <p className="text-new" id="link-final">Ainda não é cliente YoungBank? <b>Crie sua conta aqui</b></p>
@@ -30,3 +86,5 @@ function Login() {
 }
 
 export default Login;
+
+// feaa60e25e23f21eccbcd07b3e3528a3884f9e1b
